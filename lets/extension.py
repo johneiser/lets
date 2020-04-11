ef3847715870ab5486d6b4fcfa1213a78bd9b771
Justin <justin@ohneiser.com>
2020-04-10 20:54:10 -0400

    remove v1

--- lets/extension.py
+++
@@ -1,8 +0,0 @@
-from lets.logger import Logger
-
-
-class Extension(Logger, object):
-    """
-    Abstract mixin class to be inherited by each extension,
-    enabling the extension of modules with additional functionality.
-    """
--- workflows/sample.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo -n "abcd" \
-    | lets encode/base64 -v \
-    | lets decode/base64 -v
-# [+] |Base64| Running module with 4 bytes and options: {'verbose': True}
-# [+] |Base64| Running module with 8 bytes and options: {'verbose': True}
-# abcd
